36dae6d821d1fb72fa010499ff764f17dbfe2dfb
asiimwedismas <asiimwedismas@gmail.com>
2022-07-11 16:45:50 +0300

    implement map based services. closes #6

---
+++ pet-clinic-data/src/main/java/me/asiimwedismas/petclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,43 @@
+package me.asiimwedismas.petclinic.service.map;
+
+import me.asiimwedismas.petclinic.model.Owner;
+import me.asiimwedismas.petclinic.service.OwnerService;
+
+import java.util.Set;
+
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return super.map.values()
+                .stream()
+                .filter(entry ->entry.getLastName().equals(lastName))
+                .findFirst()
+                .get();
+    }
+}
---
+++ pet-clinic-data/src/main/java/me/asiimwedismas/petclinic/service/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package me.asiimwedismas.petclinic.service.map;
+
+import me.asiimwedismas.petclinic.model.Pet;
+import me.asiimwedismas.petclinic.service.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
